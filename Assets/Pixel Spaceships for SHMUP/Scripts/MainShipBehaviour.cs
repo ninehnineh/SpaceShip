3fb66fef0cef3fd62b33317ab9158bfa84e359f9
ninehnineh <103179810+ninehnineh@users.noreply.github.com>
2022-10-26 11:16:06 +0700

    Move mainship follows mouse

---
+++ Assets/Pixel Spaceships for SHMUP/Scripts/MainShipBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainShipBehaviour : MonoBehaviour
+{
+
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMousePosition();
+    }
+
+    private void FollowMousePosition()
+    {
+        transform.position = GetWorldPositionFromMouse();
+    }
+
+    private Vector2 GetWorldPositionFromMouse()
+    {
+        return mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
